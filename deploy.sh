da055a10d3d843bb48471e1e25d52275775ec9bd
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2026-02-11 10:47:58 +0100

    * add CERN helm repo for cvmfs * fix mounting of local volue

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,19 @@ KUBERNETES_VERSION='v1.34.0'
 minikube start --driver=$MINIKUBE_DRIVER --kubernetes-version=$KUBERNETES_VERSION \
 --container-runtime=docker --gpus all
 
-# Mount local directory to minikube for persistent storage
-minikube mount /scratch:/scratch &
-
 ## Create TLS secret
 kubectl create secret tls tls-secret --cert=cert.pem --key=key_pkcs1.pem 
 
-## Add persistent volume and claim for bare-metal storage
-kubectl apply -f persistent-volume.yaml
-kubectl apply -f persistent-volume-claim.yaml
+## Add scratch volume from bare-metal storage
+minikube mount /scratch:/scratch &
+kubectl apply -f scratch/persistent-volume.yaml
+kubectl apply -f scratch/persistent-volume-claim.yaml
 
 ## Add cvmfs volumes
-kubectl create namespace cvmfs
-kubectl apply -k osg-k8s-cvmfs/cvmfs-daemonset
-kubectl apply -k osg-k8s-cvmfs/cvmfs-pvcs
+helm install cvmfs-csi oci://registry.cern.ch/kubernetes/charts/cvmfs-csi --values cvmfs/cvmfs-csi-custom-values.yaml
+kubectl apply -f cvmfs/volume-pv-pvc.yaml
 
-## Add JupyterHub Helm repo
+## Add JupyterHub 
 helm upgrade --install --cleanup-on-fail \
   --install jupyterhub jupyterhub/jupyterhub \
   --namespace default \
